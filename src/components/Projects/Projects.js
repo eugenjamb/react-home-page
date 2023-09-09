bb69ed5dd28a0acec01d436156a39e09b41d2104
eugenjamb <eugenjamb@gmail.com>
2023-09-09 15:42:57 +0100

    images url fix

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => (
       {projects.map((p, i) => {
         return (
           <BlogCard key={i}>
-          <Img src={p.image} />
+          <Img src={p.image.substring(1)} />
             <TitleContent>
               <HeaderThree title>{p.title}</HeaderThree>
               <Hr />
